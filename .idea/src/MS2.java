71c1ad87e59f9f82faaf4bee8656873d84202dd7
Excelnd <ihsanmpm@gmail.com>
2020-01-30 10:54:11 +0530

    Initial commit

---
+++ .idea/src/MS2.java
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<module type="JAVA_MODULE" version="4">
+  <component name="NewModuleRootManager" inherit-compiler-output="true">
+    <exclude-output />
+    <content url="file://$MODULE_DIR$/.idea/src">
+      <sourceFolder url="file://$MODULE_DIR$/.idea/src/.idea/src" isTestSource="false" />
+      <sourceFolder url="file://$MODULE_DIR$/.idea/src/sabu" isTestSource="false" generated="true" />
+    </content>
+    <orderEntry type="inheritedJdk" />
+    <orderEntry type="sourceFolder" forTests="false" />
+  </component>
+</module>
